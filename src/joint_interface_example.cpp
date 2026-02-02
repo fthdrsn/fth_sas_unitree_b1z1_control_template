79cc15695a2de1a273ffb124744de7c1551e3159
Juancho <juanjqogm@gmail.com>
2026-02-02 15:28:32 +0000

    updated the headers

--- src/joint_interface_example.cpp
+++ src/joint_interface_example.cpp
@@ -1,3 +1,27 @@
+/*
+#    Copyright (c) 2025-2026 Adorno-Lab
+#
+#    This is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Lesser General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Lesser General Public License for more details.
+#
+#    You should have received a copy of the GNU Lesser General Public License.
+#    If not, see <https://www.gnu.org/licenses/>.
+#
+# ################################################################
+#
+#   Author: Juan Jose Quiroz Omana (email: juanjose.quirozomana@manchester.ac.uk)
+#   This example is based on https://github.com/MarinhoLab/sas_robot_driver_ur
+#
+# ################################################################
+*/
+
 #include <rclcpp/rclcpp.hpp>
 #include <sas_core/sas_clock.hpp>
 #include <sas_robot_driver/sas_robot_driver_client.hpp>
--- utils/sas_unitree_b1z1_robot_client/UnitreeB1Z1RobotClient.cpp
+++ utils/sas_unitree_b1z1_robot_client/UnitreeB1Z1RobotClient.cpp
@@ -1,3 +1,27 @@
+/*
+#    Copyright (c) 2025-2026 Adorno-Lab
+#
+#    This is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Lesser General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Lesser General Public License for more details.
+#
+#    You should have received a copy of the GNU Lesser General Public License.
+#    If not, see <https://www.gnu.org/licenses/>.
+#
+# ################################################################
+#
+#   Author: Juan Jose Quiroz Omana (email: juanjose.quirozomana@manchester.ac.uk)
+#
+# ################################################################
+*/
+
+
 #include <sas_conversions/DQ_geometry_msgs_conversions.hpp>
 #include <sas_unitree_b1z1_robot_client/UnitreeB1Z1RobotClient.hpp>
 #include <sas_core/eigen3_std_conversions.hpp>
